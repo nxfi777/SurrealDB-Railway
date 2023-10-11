31cced08be2d589445eda72b0db2e28c1f9eaa99
nxfi777 <127808926+nxfi777@users.noreply.github.com>
2023-10-11 01:05:47 +0100

    persistent disk

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/bin/surreal start -A --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} memory
+/bin/surreal start -A --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://./database
