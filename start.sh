ef1576c84d2838aea7c00a484dcbb2480ef7ffbf
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2023-12-21 14:18:48 +0000

    new stuffs

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/bin/surreal version
-/bin/surreal start -A --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://./database
+surreal start -A --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://./database
+surreal version
