b6dc839369be13ec57a3112dbe48296ab89b6e51
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2023-12-22 19:14:36 +0000

    LOGGING ENV TEST

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 surreal start -A --log=${DB_LOG} --user=${DB_USER} --pass=${DB_PASS} --bind=0.0.0.0:${DB_PORT} file://./database
 surreal version
+
+echo "DB_LOG: ${DB_LOG}"
+echo "DB_USER: ${DB_USER}"
+echo "DB_PASS: ${DB_PASS}"
+echo "DB_PORT: ${DB_PORT}"
