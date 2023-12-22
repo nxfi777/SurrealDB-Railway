acc72761fb985b5075e14a991184a2d9d4aacc60
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2023-12-22 19:40:05 +0000

    remove --auth

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 surreal version
-surreal start -A --deny-guests --log=${LOG} --auth --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://./database
+surreal start -A --deny-guests --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://./database
