62ac35e77e062cad64a331e12e0f634e46b133ed
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2023-12-22 19:44:32 +0000

    DB_ prefix remapped

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 surreal version
-surreal start -A --deny-guests --log=${LOG} --user=${USER} --pass=${PASS} --bind=0.0.0.0:${PORT} file://./database
+surreal start -A --deny-guests --log=${DB_LOG} --user=${DB_USER} --pass=${DB_PASS} --bind=0.0.0.0:${DB_PORT} file://./database
