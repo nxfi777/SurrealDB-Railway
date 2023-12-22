79b4a22aa88aff63e3291dd1f9a814961a11050c
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2023-12-22 19:26:41 +0000

    deny guests

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 surreal version
-surreal start -A --log=${DB_LOG} --auth --user=${DB_USER} --pass=${DB_PASS} --bind=0.0.0.0:${DB_PORT} file://./database
+surreal start -A --deny-guests --log=${DB_LOG} --auth --user=${DB_USER} --pass=${DB_PASS} --bind=0.0.0.0:${DB_PORT} file://./database
