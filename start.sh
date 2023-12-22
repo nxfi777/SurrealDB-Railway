ec816fae888caab3059c5b9b5ce6ccc03aebd49f
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2023-12-22 19:19:59 +0000

    Fix up

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-echo "DB_LOG: ${DB_LOG}"
-echo "DB_USER: ${DB_USER}"
-echo "DB_PASS: ${DB_PASS}"
-echo "DB_PORT: ${DB_PORT}"
+surreal version
+surreal start -A --log=${DB_LOG} --user=${DB_USER} --pass=${DB_PASS} --bind=0.0.0.0:${DB_PORT} file://./database
