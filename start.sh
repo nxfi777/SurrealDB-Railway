aa174a9ec86883a85a15cbb75ebcc72064607fab
Rafael Aslanian <127808926+nxfi777@users.noreply.github.com>
2023-12-22 19:19:01 +0000

    Only echos

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-surreal start -A --log=${DB_LOG} --user=${DB_USER} --pass=${DB_PASS} --bind=0.0.0.0:${DB_PORT} file://./database
-surreal version
-
 echo "DB_LOG: ${DB_LOG}"
 echo "DB_USER: ${DB_USER}"
 echo "DB_PASS: ${DB_PASS}"
